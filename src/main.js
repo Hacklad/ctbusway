ff953d0108f100cea248a700b5f826aa5edda2f4
Aaron Ogle <atogle@gmail.com>
2012-05-16 13:33:41 -0400

    show headway information in the tooltips

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-(function(){
+(function(routeDetails){
     var baseUrl = 'http://{s}.tiles.mapbox.com/v3/civicworks.map-akuytyon/{z}/{x}/{y}.png',
       routesUrl = 'http://a.tiles.mapbox.com/v3/civicworks.ctbusway_routes.jsonp',
       stopsUrl = 'http://a.tiles.mapbox.com/v3/civicworks.ctbusway_stops.jsonp',
@@ -12,19 +12,34 @@
       }),
       baseLayer = new L.TileLayer(baseUrl, {
         attribution: 'Map data &copy; OpenStreetMap contributors, CC-BY-SA <a href="http://mapbox.com/about/maps" target="_blank">Terms &amp; Feedback</a>'
-      });
+      }),
+      $tooltip = $('.tooltip');
 
   map.attributionControl.setPrefix('');
   map.addLayer(baseLayer).setView(new L.LatLng(41.7159, -72.7319), 12);
 
-  var initMapboxLayer = function(url, callback) {
+  var initMapboxLayer = function(url, type, formatter, callback) {
     wax.tilejson(url, function(tilejson) {
       map.addLayer(new wax.leaf.connector(tilejson));
 
       wax.leaf.interaction()
         .map(map)
         .tilejson(tilejson)
-        .on(wax.tooltip().animate(true).parent(map._container).events());
+        .on({
+          on: function(evt) {
+            var html = formatter ? formatter(evt.data) : evt.data.name;
+
+            $('.' + type + '-tooltip').html(html).show();
+            $tooltip.show();
+          },
+          off: function(evt) {
+            $('.' + type + '-tooltip').hide();
+
+            if($tooltip.children().is(':visible') === false) {
+              $tooltip.hide();
+            }
+          }
+        });
 
       if (callback) { callback(); }
     });
@@ -90,11 +105,82 @@
     });
   };
 
+  var formatStops = function(data) {
+    return '<h3>' + data.name + '</h3>';
+  };
+
+  var formatRoutes = function(data) {
+
+    // Turn the route data into an array of minutes integers
+    var getMinutes = function(keys, routeData) {
+      var i, m, mins = [];
+      // For each key
+      for(i=0; i<keys.length; i++) {
+        // Get the number
+        m = parseInt(routeData[keys[i]], 10);
+        // Add if it's really a number
+        if (isNaN(m) === false) {
+          mins.push(m);
+        }
+      }
+      return mins;
+    };
+
+    var routeData = routeDetails[data.name],
+        // Keys for the minute values. Go see route-details.js.
+        rushKeys = [
+          'Weekday AM Peak',
+          'Weekday PM Peak'
+        ],
+        otherKeys = [
+          'Weekday Midday',
+          'Weekday Evening',
+          'Saturday AM Peak',
+          'Saturday Midday',
+          'Saturday PM Peak',
+          'Saturday Evening',
+          'Sunday AM Peak',
+          'Sunday Midday',
+          'Sunday PM Peak',
+          'Sunday Evening'
+        ],
+        rushHour, minuteArray, other, min, max, headway,
+        // Default display html
+        html = '<h3>' + data.name + '</h3>';
+
+    // Not everything has additional route data
+    if (routeData) {
+      // Get the rush hours, assuming AM and PM are the samefor simplicity
+      rushHour = parseInt(routeData['Weekday AM Peak'], 10),
+      minuteArray = getMinutes(otherKeys, routeData),
+
+      // Get the min and max for non rush hour headways
+      min = Math.min.apply(null, minuteArray);
+      max = Math.max.apply(null, minuteArray);
+
+      // Support ranges for non-peak headways
+      if (min === max) {
+        other = 'every ' + max;
+      } else  {
+        other = 'between ' + min + ' and ' + max;
+      }
+
+      // Better English when peak and nonpeak times are the same
+      if (min === max && max === rushHour) {
+         html += 'Runs every ' + rushHour + ' minutes.';
+      } else {
+        html += 'Runs every ' + rushHour + ' minutes during rush hour and ' + other + ' minutes at other times.';
+      }
+    }
+
+    return html;
+  };
+
   initLocation();
 
-  initMapboxLayer(routesUrl, function(){
-    initMapboxLayer(stopsUrl);
+  initMapboxLayer(routesUrl, 'routes', formatRoutes, function(){
+    initMapboxLayer(stopsUrl, 'stops', formatStops);
   });
 
   bindEvents();
-})();
+})(routeDetails);
