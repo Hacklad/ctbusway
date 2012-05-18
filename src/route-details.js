f9304898744b8b1bea0302154ff58ce861d42d15
Paul Winkler <slinkp@gmail.com>
2012-05-18 01:22:46 -0400

    More route info for busway station pops

--- src/route-details.js
+++ src/route-details.js
@@ -4,6 +4,7 @@ var routeDetails = {
     "One Way Mileage": "3.1 miles",
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnight) and 12 hours (8 am to 8pm) on Sundays.",
     "Service Pattern": "The CCSU shuttle will operate in both directions, laying over at Cedar or East depending upon the direction of travel. Bus stops will be established along the route at convenient locations near important buildings on campus.",
+    "stops": ["Cedar Street", "East Street"],
     "Weekday AM Peak": "15",
     "Weekday Midday": "20",
     "Weekday PM Peak": "15",
@@ -22,6 +23,7 @@ var routeDetails = {
     "One Way Mileage": "3.4 miles",
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnight) and 12 hours (8 am to 8pm) on Sundays.",
     "Service Pattern": "The CCSU shuttle will operate in both directions, laying over at Cedar or East depending upon the direction of travel. Bus stops will be established along the route at convenient locations near important buildings on campus.",
+    "stops": ["Sigourney Street"],
     "Weekday AM Peak": "15",
     "Weekday Midday": "20",
     "Weekday PM Peak": "15",
@@ -40,6 +42,7 @@ var routeDetails = {
     "One Way Travel Time": "30 minutes",
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnight) and 12 hours (8 am to 8pm) on Sundays.",
     "Service Pattern": "The route will operate in local service stopping at all bus stops along its route.",
+    "stops": ["Downtown New Britain", "Newington Junction", "Cedar Street"],
     "Weekday AM Peak": "30",
     "Weekday Midday": "60",
     "Weekday PM Peak": "30",
@@ -55,6 +58,7 @@ var routeDetails = {
   },
   "C4 West Hartford - Bishops Corner Connector": {
     "Service Pattern": "This route would operate in local service stopping at all bus stops along its routing.",
+    "stops": ["Flatbush Avenue"],
     "One Way Travel Time": "21 minutes",
     "One Way Mileage": "5.4 mile",
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnight) and 12 hours (8 am to 8pm) on Sundays.",
@@ -76,7 +80,7 @@ var routeDetails = {
     "One Way Travel Time": "70 minutes",
     "One Way Mileage": "34.4 miles",
     "Service Pattern": "All trips operate from the Waterbury train station via the transit center, Exit 23 park and ride, Plantsville park and ride, New Britain station, Sigourney Street station and then via the downtown Hartford express bus loop (Farmington, Asylum, Main, Capitol) making all local stops when on-street. The loop would be operated clockwise in the morning and counterclockwise in the afternoon.",
-    "stops": ["New Britain", "Sigourney Street", "Union Station"],
+    "stops": ["Union Station", "Sigourney Street", "Downtown New Britain"],
     "Service Span": "This route will operate only on weekdays during AM and PM peak periods, from 6am to 9am and from 3pm to 6pm.",
     "Weekday AM Peak": "30",
     "Weekday Midday": "",
@@ -97,7 +101,7 @@ var routeDetails = {
     "One Way Travel Time": "56 minutes",
     "Service Span": "This route will operate only on weekdays during AM and PM peak periods, from 6am to 9am and from 3pm to 6pm.",
     "Service Pattern": "All trips operate from the Exit 26 park-and-ride, Plantsville park-and-ride, New Britain station, Sigourney Street station and then via the downtown Hartford express bus loop (Farmington, Asylum, Main, Capitol) making all local stops when on-street. The loop would be operated clockwise in the morning and counterclockwise in the afternoon.",
-    "stops": ["New Britain", "Sigourney Street", "Union Station"],
+    "stops": ["Downtown New Britain", "Sigourney Street", "Union Station"],
     "Weekday AM Peak": "30",
     "Weekday Midday": "",
     "Weekday PM Peak": "30",
@@ -115,7 +119,7 @@ var routeDetails = {
     "One Way Mileage": "34.4 miles",
     "One Way Travel Time": "70 minutes",
     "Service Pattern": "All trips operate between Waterbury, Exit 23 park-and-ride, Exit 26 park-and-ride, Plantsville park-and-ride, New Britain station, Sigourney Street station and then via the downtown Hartford express bus loop (Farmington, Asylum, Main, Capitol) making all local stops when on-street. The loop would be operated clockwise in the morning and counterclockwise in the afternoon and evening.",
-    "stops": ["New Britain", "Sigourney Street", "Union Station"],
+    "stops": ["Downtown New Britain", "Sigourney Street", "Union Station"],
     "Service Span": "This route will operate only during off-peak periods Monday through Sunday.",
     "Weekday AM Peak": "",
     "Weekday Midday": "60",
@@ -154,7 +158,7 @@ var routeDetails = {
 
   "West Hartford Place via Park and Kane St": {
     "Service Pattern": "All trips operate in local transit service between West Hartford Place mall and downtown Hartford making all local stops.",
-    "stops": ["Park Street"],
+    "stops": ["Park Street", "Flatbush Avenue"],
     "One Way Travel Time": "29 minutes",
     "One Way Mileage": "4.1 miles",
     "Service Span": "This service operates only during midday periods only (9am - 3pm) Monday through Friday.",
@@ -215,7 +219,7 @@ var routeDetails = {
 
   "Charter Oak Marketplace West Hartford Place via NB Ave": {
     "Service Pattern": "All trips operate in local transit service between West Hartford Place mall and downtown Hartford making all local stops.",
-    "stops": [],
+    "stops": ["Flatbush Avenue"],
     "One Way Travel Time": "39 minutes",
     "One Way Mileage": "5.2 miles",
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnight) and 12 hours (8am to 8pm) on Sundays.",
@@ -235,7 +239,7 @@ var routeDetails = {
 
   "Elmwood via New Britain Ave": {
     "Service Pattern": "All trips operate in local transit service between Elmwood and downtown Hartford making all local stops.",
-    "stops": [],
+    "stops": ["Elmwood"],
     "One Way Travel Time": "34 minutes",
     "One Way Mileage": "4.9 miles",
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnight) and 12 hours (8am to 8pm) on Sundays.",
@@ -256,7 +260,7 @@ var routeDetails = {
 
   "Capitol - Berlin Turnpike via Willard": {
     "Service Pattern": "All trips operate in local transit service between Berlin Turnpike and downtown Hartford making all local stops.",
-    "stops": [],
+    "stops": ["Newington Junction"],
     "One Way Travel Time": "41 minutes",
     "One Way Mileage": "11.9 miles",
     "Service Span": "Monday through Saturday from 6am to 6pm. No service during evenings and on Sundays.",
@@ -276,6 +280,7 @@ var routeDetails = {
 
   "Arch Street": {
     "Service Pattern": "All trips operate in local transit service between Meriden and New Britain station making all local stops.",
+    "stops": ["Downtown New Britain"],
     "One Way Travel Time": "30 minutes",
     "One Way Mileage": "10.1 miles",
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnight) and 12 hours (8 am to 8pm) on Sundays.",
@@ -295,6 +300,7 @@ var routeDetails = {
 
   "Burritt Street": {
     "Service Pattern": "All trips operate in local transit service between Slater Road and New Britain station making all local stops.",
+    "stops": ["Downtown New Britain"],
     "One Way Travel Time": "20 minutes",
     "One Way Mileage": "5.6 miles",
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnight) and 12 hours (8 am to 8pm) on Sundays.",
@@ -314,6 +320,7 @@ var routeDetails = {
 
   "Corbin Avenue": {
     "Service Pattern": "All trips operate in local transit service between Tunxis Community College and New Britain station making all local stops.",
+    "stops": ["Downtown New Britain"],
     "One Way Travel Time": "30 minutes",
     "One Way Mileage": "9.4 miles",
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnight) and 12 hours (8 am to 8pm) on Sundays.",
@@ -333,6 +340,7 @@ var routeDetails = {
 
   "Farmington Avenue": {
     "Service Pattern": "All trips operate in local transit service between the UConn Health Center and New Britain station making all local stops.",
+    "stops": ["Downtown New Britain"],
     "One Way Travel Time": "30 minutes",
     "One Way Mileage": "7.0 miles",
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnight) and 12 hours (8 am to 8pm) on Sundays.",
@@ -352,6 +360,7 @@ var routeDetails = {
 
   "Oak Street": {
     "Service Pattern": "All trips operate in local transit service between Cedar Street station and New Britain station making all local stops.",
+    "stops": ["Downtown New Britain", "Cedar Street"],
     "One Way Travel Time": "15 minutes",
     "One Way Mileage": "3.8 miles",
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnight) and 12 hours (8 am to 8pm) on Sundays.",
@@ -371,6 +380,7 @@ var routeDetails = {
 
   "Plainville/Bristol": {
     "Service Pattern": "All trips operate in local transit service between Forestville and New Britain station making all local stops.",
+    "stops": ["Downtown New Britain"],
     "One Way Travel Time": "30 minutes",
     "One Way Mileage": "6.6 miles",
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnight) and 12 hours (8 am to 8pm) on Sundays.",
@@ -389,6 +399,7 @@ var routeDetails = {
 
   "Berlin Turnpike": {
     "Service Pattern": "All trips operate in local transit service between the Berlin Turnpike and New Britain station making all local stops.",
+    "stops": ["Downtown New Britain"],
     "One Way Travel Time": "30 minutes",
     "One Way Mileage": "7.4 miles",
     "Service Span": "Weekday peak periods only, from 6am to 9am and from 3pm to 6pm. ",
@@ -408,6 +419,7 @@ var routeDetails = {
 
   "Berlin Turnpike (Cromwell)": {
     "Service Pattern": "All trips operate in local transit service between the Cromwell Wal-Mart and New Britain station making all local stops.",
+    "stops": ["Downtown New Britain"],
     "One Way Travel Time": "45 minutes",
     "One Way Mileage": "10.5 miles",
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnight) and 12 hours (8 am to 8pm) on Sundays.",
@@ -427,6 +439,7 @@ var routeDetails = {
 
   "Combined South and East Streets": {
     "Service Pattern": "All trips operate in local transit service between Corbin Russwin and New Britain station and on to East Street making all local stops.",
+    "stops": ["Downtown New Britain", "East Main Street"],
     "One Way Mileage": "7.6 miles",
     "One Way Travel Time": "30 minutes",
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnight) and 12 hours (8 am to 8pm) on Sundays.",
@@ -448,7 +461,7 @@ var routeDetails = {
     "One Way Travel Time": "28 minutes",
     "One Way Mileage": "10.1 miles",
     "Service Pattern": "This route will make all local station stops along its route including: New Britain, East Main, East Street, Cedar Street, Newington Jct., Elmwood, Flatbush Avenue, Kane Street, Park Street, Sigourney Street, Union Station and in downtown Hartford, Ann Street, Trumbull Street and Main Street. The route will travel exclusively on the busway except for the downtown loop from Union Station to Main Street.",
-    "stops": ["New Britain", "East Main", "East Street", "Cedar Street", "Newington Junction", "Elmwood", "Flatbush Avenue", "Kane Street", "Park Street", "Sigourney Street", "Union Station"],
+    "stops": ["Downtown New Britain", "East Main Street", "East Street", "Cedar Street", "Newington Junction", "Elmwood", "Flatbush Avenue", "Kane Street", "Park Street", "Sigourney Street", "Union Station"],
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnitght) and 12 hours (8am to 8pm) on Sundays.",
     "Weekday AM Peak": "12",
     "Weekday Midday": "20",
@@ -469,6 +482,7 @@ var routeDetails = {
     "One Way Travel Time": "50 minutes",
     "One Way Mileage": "20.1 miles",
     "Service Pattern": "From Bristol this route will make limited stops along Rt. 72 to New Britain and the busway. Once on the busway, this route will make all local station stops along its route including: New Britain, East Main, East Street, Cedar Street, Newington Jct., Elmwood, Flatbush Avenue, Kane Street, Park Street, Sigourney Street, Union Station and in downtown Hartford, Ann Street, Trumbull Street and Main Street. The route will travel exclusively on the busway except for the downtown loop from Union Station to Main Street.",
+    "stops": ["Downtown New Britain", "East Main Street", "East Street", "Cedar Street", "Newington Junction", "Elmwood", "Flatbush Avenue", "Kane Street", "Park Street", "Sigourney Street", "Union Station"],
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnitght) and 12 hours (8am to 8pm) on Sundays.",
     "Weekday AM Peak": "12",
     "Weekday Midday": "20",
@@ -488,6 +502,7 @@ var routeDetails = {
     "One Way Travel Time": "44 minutes",
     "One Way Mileage": "11.5 miles",
     "Service Pattern": "This route will stop at all local bus stops along its route from downtown New Britain to Westfarms and then through Elmwood to the busway. After it enters the busway at Oakwood, it will make all local busway stops at Flatbush, Kane Street, Park Street, Sigourney Street, and Union Station. On-street in downtown Hartford it will stop at Anne, Trumbull, and Main Streets.",
+    "stops": ["Downtown New Britain", "Elmwood", "Flatbush Avenue", "Kane Street", "Park Street", "Sigourney Street", "Union Station"],
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnitght) and 16 hours (8am to Midnight) on Sundays.",
     "Weekday AM Peak": "18",
     "Weekday Midday": "20",
@@ -506,6 +521,7 @@ var routeDetails = {
     "One Way Travel Time": "43 minutes",
     "One Way Mileage": "9.1 miles",
     "Service Pattern": "This route will stop at all local bus stops along its route from UConn Health Center to Westfarms and then through Elmwood to the busway. After it enters the busway at Oakwood, it will make all local busway stops at Flatbush, Kane Street, Park Street, Sigourney Street, and Union Station. On-street in downtown Hartford it will stop at Anne, Trumbull, and Main Streets.",
+    "stops": ["Elmwood", "Flatbush Avenue", "Kane Street", "Park Street", "Sigourney Street", "Union Station"],
     "Service Span": "Seven days a week, 18 hours a day on Monday to Saturday (6am to Midnitght) and 12 hours (8am to 8pm) on Sundays.",
     "Weekday AM Peak": "18",
     "Weekday Midday": "20",
@@ -524,6 +540,7 @@ var routeDetails = {
     "One Way Travel Time": "16 minutes",
     "One Way Mileage": "6.0 miles",
     "Service Pattern": "This route makes all local stops along the busway starting in Newington Junction and including Elmwood, Flatbush, Kane Street, Park Street, Sigourney Street, and Union Station. In downtown Hartford it stops on-street at Ann, Trumbull, and Main.",
+    "stops": ["Newington Junction", "Elmwood", "Flatbush Avenue", "Kane Street", "Park Street", "Sigourney Street", "Union Station"],
     "Service Span": "This route will operate only on weekdays during AM and PM peak periods, from 6am to 9am and from 3pm to 6pm.",
     "Weekday AM Peak": "18",
     "Weekday Midday": "",
