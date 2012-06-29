b70385ee47a9c64808c088af71a44efcfa9200af
Aaron Ogle <atogle@gmail.com>
2012-06-29 08:00:33 -0400

    update routes mapbox url

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 (function(routeDetails){
     var baseUrl = 'http://{s}.tile.cloudmade.com/d253021886024e50adec434f02cbf0b5/63237/256/{z}/{x}/{y}.png',
-      routesUrl = 'http://a.tiles.mapbox.com/v3/openplans.ctbusway_routes_628445.jsonp',
+      routesUrl = 'http://a.tiles.mapbox.com/v3/openplans.ctbusway_routes_629757.jsonp',
       stopsUrl = 'http://a.tiles.mapbox.com/v3/openplans.ctbusway_stops_27511.jsonp',
       map = new L.Map('map', {
         minZoom: 11,
