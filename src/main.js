cd045f04cbc5fa8207fc0b4fc03cab812ba4ecd0
Paul Winkler <slinkp@gmail.com>
2012-07-05 17:51:25 -0400

    Use latest tiles

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 (function(routeDetails){
     var baseUrl = 'http://{s}.tile.cloudmade.com/d253021886024e50adec434f02cbf0b5/63237/256/{z}/{x}/{y}.png',
-      routesUrl = 'http://a.tiles.mapbox.com/v3/openplans.ctbusway_routes_6291830.jsonp',
+      routesUrl = 'http://a.tiles.mapbox.com/v3/openplans.ctbusway_routes_751744.jsonp',
       stopsUrl = 'http://a.tiles.mapbox.com/v3/openplans.ctbusway_stops_6291125.jsonp',
       map = new L.Map('map', {
         minZoom: 11,
